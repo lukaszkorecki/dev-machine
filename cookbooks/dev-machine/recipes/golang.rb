1365a8bdf98ec20987581355b32fc2a03ec1ed76
Łukasz Korecki <lukasz@coffeesounds.com>
2013-09-14 10:31:18 +0100

    fix recipes by removing redundant actions

--- cookbooks/dev-machine/recipes/golang.rb
+++ cookbooks/dev-machine/recipes/golang.rb
@@ -1,16 +1,14 @@
 include_recipe "dev-machine::ppa"
 
 execute "Add redis-server PPA" do
-  action :run
   command "apt-add-repository ppa:gophers/go"
 end
 
 execute "apt-get update" do
-  command "apt-get update"
   action :run
 end
 
-package "golang-stable" do
+package "golang" do
   action :install
 end
 
