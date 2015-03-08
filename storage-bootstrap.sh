75da1cebd8d526b7e116acf9de26775a8bb7e9b6
Łukasz Korecki <lukasz@korecki.me>
2015-03-08 16:16:39 +0000

    ES needs java of course

--- storage-bootstrap.sh
+++ storage-bootstrap.sh
@@ -6,13 +6,28 @@ _q() {
   $* > /dev/null
 }
 
-curl https://packages.elasticsearch.org/GPG-KEY-elasticsearch > /tmp/es-key
-apt-key add /tmp/es-key
+if grep elasticsearch /etc/apt/sources.list; then
+  echo "ES installed"
+else
+  curl https://packages.elasticsearch.org/GPG-KEY-elasticsearch > /tmp/es-key
+  apt-key add /tmp/es-key
 
-add-apt-repository "deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main"
-apt-get update
+  add-apt-repository "deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main"
+  apt-get update
+fi
 
-apt-get install -y -q elasticsearch \
+apt-get install -y -q  \
   postgresql-9.3 \
   redis-server \
-  redis-tools
+  redis-tools \
+  ufw \
+  openjdk-7-jre-headless \
+  elasticsearch
+
+
+# open up ports
+ufw allow ssh
+ufw allow 5432
+ufw allow 9200
+ufw allow 6379
+yes | ufw enable
