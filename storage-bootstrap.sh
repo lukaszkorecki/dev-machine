5b1e9655ba906fbcc9748f25c2195e243505ffb3
Łukasz Korecki <lukasz@korecki.me>
2015-05-04 16:26:01 +0100

    Upgrade to ES 1.5.

--- storage-bootstrap.sh
+++ storage-bootstrap.sh
@@ -8,11 +8,11 @@ _q() {
 
 if grep elasticsearch /etc/apt/sources.list; then
   echo "ES installed"
-else
+# else
   curl https://packages.elasticsearch.org/GPG-KEY-elasticsearch > /tmp/es-key
   apt-key add /tmp/es-key
 
-  add-apt-repository "deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main"
+  add-apt-repository "deb http://packages.elasticsearch.org/elasticsearch/1.5/debian stable main"
   apt-get update
 fi
 
